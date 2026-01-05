a928bbbb73bc6e870242f6d293d9dbeaf1730013
Rha Bragais <rha.bragais@gmail.com>
2026-01-05 11:01:40 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-399912310-23c48",
+  "appId": "1:902135982838:web:120bbdb354f85b35eddad7",
+  "apiKey": "AIzaSyDLk_1j0o1BH9J1pSZw0SGTEqToFo7ISYo",
+  "authDomain": "studio-399912310-23c48.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "902135982838"
+};
