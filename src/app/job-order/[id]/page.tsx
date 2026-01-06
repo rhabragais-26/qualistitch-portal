09329e75b3ec5eb4d5ee8f75e113723ba4c9b69f
Rha Bragais <rha.bragais@gmail.com>
2026-01-06 13:48:21 +0000

    add another 0.5 inch space after the "Next >" button

--- src/app/job-order/[id]/page.tsx
+++ src/app/job-order/[id]/page.tsx
@@ -535,7 +535,7 @@ export default function JobOrderPage() {
                     variant="outline"
                     onClick={() => setCurrentPage(p => Math.min(totalPages, p + 1))}
                     disabled={currentPage === totalPages}
-                    style={{ marginRight: '0.5in' }}
+                    style={{ marginRight: '1in' }}
                 >
                     Next
                     <ChevronRight className="ml-2 h-4 w-4" />
