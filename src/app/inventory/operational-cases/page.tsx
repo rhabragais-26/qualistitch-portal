cc8d7336343ec41259c09bc4d82806157bc8d382
Rha Bragais <rha.bragais@gmail.com>
2026-01-07 11:02:36 +0000

    move the recorded cases section a bit to the right for the record Operat

--- src/app/inventory/operational-cases/page.tsx
+++ src/app/inventory/operational-cases/page.tsx
@@ -9,11 +9,11 @@ export default function OperationalCasesPage() {
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-1 w-full p-4 sm:p-6 lg:p-8">
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 items-start">
-            <div className="lg:col-span-1 lg:sticky lg:top-24">
+        <div className="grid grid-cols-1 lg:grid-cols-5 gap-8 items-start">
+            <div className="lg:col-span-2 lg:sticky lg:top-24">
                  <OperationalCasesForm />
             </div>
-            <div className="lg:col-span-2">
+            <div className="lg:col-span-3">
                 <RecordedCasesList />
             </div>
         </div>
