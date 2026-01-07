efab39f747aaa30bb9a5ae22abd77dad14e76a84
Rha Bragais <rha.bragais@gmail.com>
2026-01-07 23:28:37 +0000

    remove the large margin on top of the inventory summary page

--- src/app/inventory/summary/page.tsx
+++ src/app/inventory/summary/page.tsx
@@ -5,7 +5,7 @@ export default function InventorySummaryPage() {
   return (
     <div className="flex flex-col h-screen">
       <Header />
-      <main className="flex-1 w-full p-4 sm:p-6 lg:p-8 overflow-hidden">
+      <main className="flex-1 w-full overflow-hidden">
         <InventorySummaryTable />
       </main>
     </div>
