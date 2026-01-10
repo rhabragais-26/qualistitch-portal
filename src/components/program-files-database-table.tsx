fc8ba65e3884eebd17d4dfb68dbe20e0c12b6827
Rha Bragais <rha.bragais@gmail.com>
2026-01-10 21:11:11 +0000

    Show file names under DST files column from Program Files Database but s

--- src/components/program-files-database-table.tsx
+++ src/components/program-files-database-table.tsx
@@ -255,17 +255,17 @@ const ProgramFilesDatabaseTableMemo = React.memo(function ProgramFilesDatabaseTa
                         <div className="flex flex-col gap-1 items-start mx-auto w-fit">
                           {lead.layouts?.[0]?.finalLogoDst?.map((file, i) => file?.url && file?.name && (
                             <Button key={`dst-logo-${i}`} variant="link" size="sm" className="h-auto p-0 text-xs" onClick={() => handleDownload(file.url, file.name)}>
-                              <Download className="mr-1 h-3 w-3" /> Logo {i+1} (DST)
+                              <Download className="mr-1 h-3 w-3" /> {file.name}
                             </Button>
                           ))}
                           {lead.layouts?.[0]?.finalBackDesignDst?.map((file, i) => file?.url && file?.name && (
                             <Button key={`dst-back-${i}`} variant="link" size="sm" className="h-auto p-0 text-xs" onClick={() => handleDownload(file.url, file.name)}>
-                               <Download className="mr-1 h-3 w-3" /> Back Design {i+1} (DST)
+                               <Download className="mr-1 h-3 w-3" /> {file.name}
                             </Button>
                           ))}
                            {lead.layouts?.[0]?.finalNamesDst?.map((file, i) => file?.url && file?.name && (
                             <Button key={`dst-name-${i}`} variant="link" size="sm" className="h-auto p-0 text-xs" onClick={() => handleDownload(file.url, file.name)}>
-                               <Download className="mr-1 h-3 w-3" /> Name {i+1} (DST)
+                               <Download className="mr-1 h-3 w-3" /> {file.name}
                             </Button>
                           ))}
                         </div>
