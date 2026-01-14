ae1d7eab5e749e35fbe4f985c55de5851a31ea40
Rha Bragais <rha.bragais@gmail.com>
2026-01-14 13:21:35 +0000

    fix the issue

--- next.config.ts
+++ next.config.ts
@@ -36,6 +36,12 @@ const nextConfig: NextConfig = {
       },
        {
         protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+       {
+        protocol: 'https',
         hostname: 'encrypted-tbn0.gstatic.com',
         port: '',
         pathname: '/**',
