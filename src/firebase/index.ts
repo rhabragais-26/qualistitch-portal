ffad5616a006b37d6b7149a3a0d0aa2aac7d49b5
Rha Bragais <rha.bragais@gmail.com>
2026-01-14 20:06:36 +0000

    Try fixing this error: `Console Error: [2026-01-14T20:04:33.124Z] @fireb

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -3,15 +3,15 @@
 import { firebaseConfig } from '@/firebase/config';
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore'
+import { getFirestore, initializeFirestore } from 'firebase/firestore'
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
   const isInitialized = getApps().length > 0;
   const app = isInitialized ? getApp() : initializeApp(firebaseConfig);
   const auth = getAuth(app);
-  // Reverted to standard Firestore initialization to resolve internal SDK error.
-  const firestore = getFirestore(app);
+  // Ensure that we get a new Firestore instance only if one doesn't already exist.
+  const firestore = initializeFirestore(app, {});
 
 
   // NOTE: Emulator connections have been removed to resolve connectivity issues
