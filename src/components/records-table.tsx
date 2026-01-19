af575fc8168ea19a5322b63715a8bff29ac7c684
Rha Bragais <rha.bragais@gmail.com>
2026-01-19 12:42:52 +0000

    the page freezes upon navigating from create new order page to recorded

--- src/components/records-table.tsx
+++ src/components/records-table.tsx
@@ -286,7 +286,7 @@ export function RecordsTable({ isReadOnly }: { isReadOnly: boolean }) {
   const { toast } = useToast();
 
   const leadsQuery = useMemoFirebase(() => firestore ? query(collection(firestore, 'leads')) : null, [firestore]);
-  const { data: leads, isLoading: areLeadsLoading, error: leadsError, refetch: refetchLeads } = useCollection<Lead>(leadsQuery, leadSchema, { listen: false });
+  const { data: leads, isLoading: areLeadsLoading, error: leadsError, refetch: refetchLeads } = useCollection<Lead>(leadsQuery, undefined, { listen: false });
   
   const inventoryQuery = useMemoFirebase(() => firestore ? query(collection(firestore, 'inventory')) : null, [firestore]);
   const { data: inventoryItems, isLoading: isInventoryLoading, error: inventoryError } = useCollection<InventoryItem>(inventoryQuery, inventoryItemSchema, { listen: false });
@@ -542,4 +542,3 @@ export function RecordsTable({ isReadOnly }: { isReadOnly: boolean }) {
     </Card>
   );
 }
-
