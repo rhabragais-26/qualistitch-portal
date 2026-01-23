46d2cc2f708c2e7a0ca8e9fe80067db6deb600a7
Rha Bragais <rha.bragais@gmail.com>
2026-01-23 22:55:55 +0000

    add a border to User Management card same with the Product & Pricing Man

--- src/components/admin-users-table.tsx
+++ src/components/admin-users-table.tsx
@@ -281,7 +281,7 @@ export function AdminUsersTable() {
 
   return (
     <>
-      <Card className="w-full shadow-xl animate-in fade-in-50 duration-500 bg-white text-black h-full flex flex-col border-none">
+      <Card className="w-full shadow-xl animate-in fade-in-50 duration-500 bg-white text-black h-full flex flex-col">
         <CardHeader>
           <div className="flex justify-between items-center">
             <div>
