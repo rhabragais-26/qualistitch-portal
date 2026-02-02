0fa9e4e36f688984a46577366b5f83187aea6a5a
Rha Bragais <rha.bragais@gmail.com>
2026-02-02 12:27:54 +0000

    please fix the error for src/app/job-order/[id]/print/layout.tsx (14:7)

--- src/app/job-order/[id]/print/layout.tsx
+++ src/app/job-order/[id]/print/layout.tsx
@@ -10,10 +10,8 @@ export default function PrintLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body>
-        {children}
-      </body>
-    </html>
+    <>
+      {children}
+    </>
   );
 }
