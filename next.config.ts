96d024940a16beade7c0e81d09d6140b4d32a1ae
Rha Bragais <rha.bragais@gmail.com>
2026-04-06 05:39:34 +0000

    Try fixing this error: `Console Error: Body exceeded 10mb limit. To conf

--- next.config.ts
+++ next.config.ts
@@ -16,7 +16,7 @@ const nextConfig: NextConfig = {
 
   experimental: {
     serverActions: {
-      bodySizeLimit: "10mb", // increase if needed (ex: "6mb")
+      bodySizeLimit: "20mb", // increase if needed (ex: "6mb")
     },
   },
 
